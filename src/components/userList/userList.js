cd745334b771503232ce3f78e75951ca15e54d9c
Kevin Mathmann <kevin.mathmann@gmail.com>
2017-09-05 17:29:49 +0200

    changes save userListWrapper outside of render method

--- src/components/userList/userList.js
+++ src/components/userList/userList.js
@@ -1,6 +1,8 @@
 import getUserElement from '../user/user';
 import userStore from '../../stores/userStore';
 
+const userListWrapper = document.querySelector('#usersList');
+
 export default () => {
     render();
     userStore.subscribe(() => render());
@@ -9,11 +11,10 @@ export default () => {
 const render = () => {
     const users = userStore.getUsers();
 
-    const wrapper = document.querySelector('#usersList');
-    wrapper.innerHTML = '';
+    userListWrapper.innerHTML = '';
 
     for (let i = 0, l = users.length; i < l; i++) {
-        wrapper.appendChild(
+        userListWrapper.appendChild(
             getUserElement(users[i], () => {
                 userStore.removeUser(users[i].userId);
             })
